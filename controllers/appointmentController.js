9d01ae83b4964bbb5d3fd569240f07704d6c5a63
youssef sheashia <118205824+youssef-sheashia@users.noreply.github.com>
2026-06-15 02:17:28 +0300

    solve errors

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,21 +1,25 @@
+import catchAsync from "../utils/catchAsync.js";
 export const getMyAppointments = catchAsync(async (req, res, next) => {
   const { date, startDate, endDate, month, year } = req.query;
 
   let filter = { doctor: req.user._id };
 
-  if (date) {// for day date=2026-04-13
+  if (date) {
+    // for day date=2026-04-13
     const start = new Date(date);
     start.setHours(0, 0, 0, 0);
     const end = new Date(date);
     end.setHours(23, 59, 59, 999);
     filter.date = { $gte: start, $lte: end };
-  } else if (startDate && endDate) {// month startDate=2026-04-13&endDate=2026-04-19
+  } else if (startDate && endDate) {
+    // month startDate=2026-04-13&endDate=2026-04-19
     const start = new Date(startDate);
     start.setHours(0, 0, 0, 0);
     const end = new Date(endDate);
     end.setHours(23, 59, 59, 999);
     filter.date = { $gte: start, $lte: end };
-  } else if (month && year) {// for year month=4&year=2026
+  } else if (month && year) {
+    // for year month=4&year=2026
     const start = new Date(year, month - 1, 1);
     const end = new Date(year, month, 0, 23, 59, 59);
     filter.date = { $gte: start, $lte: end };
--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import authnticate from '../middlewares/authenticate';
-import { restrictTo } from '../controllers/authController';
-import {appointmentQuerySchema} from '../utils/validators';
-import { validateQuery } from '../middlewares/validate';
-import { getMyAppointments } from '../controllers/appointmentController';
+import express from "express";
+import authnticate from "../middlewares/authenticate.js";
+import { restrictTo } from "../controllers/authController.js";
+import { appointmentQuerySchema } from "../utils/validators.js";
+import { validateQuery } from "../middlewares/validate.js";
+import { getMyAppointments } from "../controllers/appointmentController.js";
 const router = express.Router();
 
 router.use(authnticate);
-router.use(restrictTo('doctor'));
-router.get('/appointments', validateQuery(appointmentQuerySchema), getMyAppointments);
+router.use(restrictTo("doctor"));
+router.get(
+  "/appointments",
+  validateQuery(appointmentQuerySchema),
+  getMyAppointments,
+);
 export default router;
