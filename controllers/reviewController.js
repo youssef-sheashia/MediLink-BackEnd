7759ab388953c68a865eb4e5c1be24761f50fd00
youssef sheashia <118205824+youssef-sheashia@users.noreply.github.com>
2026-06-22 21:38:17 +0300

    handel rating

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -5,30 +5,36 @@ import Review from "../models/reviewModel.js";
 import Appointment from "../models/appointmentModel.js";
 
 export const createReview = catchAsync(async (req, res, next) => {
-  const { appointmentId, stars, comment } = req.body;
+  const { stars, comment } = req.body;
   const patientId = req.user._id;
 
   // 1. get the appointment and make sure it belongs to this patient
-  const appointment = await Appointment.findById(appointmentId);
+  const appointment = await Appointment.findOne({
+    patient: patientId,
+    status: "مكتمل",
+    isRated: false,
+  });
 
-  if (!appointment)
-    return next(new AppError("Appointment not found", 404));
+  if (!appointment) return next(new AppError("Appointment not found", 404));
 
   if (appointment.patient.toString() !== patientId.toString())
     return next(new AppError("This appointment does not belong to you", 403));
 
   // 2. only allow reviews for completed appointments
   if (appointment.status !== "مكتمل")
-    return next(new AppError("You can only review a completed appointment", 400));
+    return next(
+      new AppError("You can only review a completed appointment", 400),
+    );
 
   // 3. create the review — post save hook will auto recalculate doctor rating
   const review = await Review.create({
     patient: patientId,
     doctor: appointment.doctor,
-    appointment: appointmentId,
+    appointment: appointment._id,
     stars,
     comment,
   });
+  await Appointment.findByIdAndUpdate(appointment._id, { isRated: true });
 
   res.status(201).json({
     status: "success",
@@ -39,7 +45,8 @@ export const createReview = catchAsync(async (req, res, next) => {
 export const getDoctorReviews = catchAsync(async (req, res, next) => {
   const { doctorId } = req.params;
   const { page = 1, limit = 10 } = req.query;
-  if(!mongoose.Types.ObjectId.isValid(doctorId)) return next(new AppError("Invalid id",400));
+  if (!mongoose.Types.ObjectId.isValid(doctorId))
+    return next(new AppError("Invalid id", 400));
   const reviews = await Review.find({ doctor: doctorId })
     .populate("patient", "firstName lastName photo")
     .sort({ createdAt: -1 })
